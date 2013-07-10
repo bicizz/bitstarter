f73e31f953e61f17b5e527db19ec4b4370e99426
Martyn <bicizz@yahoo.com>
2013-07-10 20:49:54 +0000

    modified js to use index file

--- web.js
+++ web.js
@@ -1,10 +1,15 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+
+fs.readFile('index.html', function(err, contents) {
+    response.write(contents);
+    response.end();
+  });
+
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
